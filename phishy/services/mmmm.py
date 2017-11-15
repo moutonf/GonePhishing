081e6639b598a4c7c73e251d951ff90c215c241e
alex <alexramantswana@gmail.com>
2017-11-15 13:54:50 +0200

    updated again

---
+++ phishy/services/mmmm.py
@@ -0,0 +1,21 @@
+# import datetime
+# from .models import *
+#
+#
+#
+# def test():
+#      group_names
+#
+#
+# def todays():
+#      time=str(datetime.datetime.now())
+#      return time[:-7]
+#
+#
+# print(todays())
+
+# test=groups.objects.raw('''SELECT g.group_id,g.group_name, c.campaign_id ,c.campaign_name FROM services_groups g, services_campaign c ''')
+        # print(test.columns)
+        #
+        # for t in test:
+        #     print(t.group_name)
